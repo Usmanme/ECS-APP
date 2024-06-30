ee45fa44f5e2a0c5a13be222d390002177d3b78a
Usman Yousaf <67965449+Usmanme@users.noreply.github.com>
2024-06-30 16:10:44 +0500

    backend work

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -4,12 +4,9 @@
     <header>
         <!-- Sidebar -->
         @include('layouts.partials.left-sidebar')
-        <!-- Sidebar -->
         <!-- Navbar -->
         @include('layouts.partials.nav')
-        <!-- Navbar -->
     </header>
-    <!-- NAVBAR ENDS -->
     <!--Main layout-->
     <main class="ecs-main-body">
         <div class="container-fluid pt-4">
@@ -44,22 +41,21 @@
                             <!-- Tabs navs -->
                             <ul class="nav nav-tabs mb-3 ecs-tab-main" id="ex1" role="tablist">
                                 <li class="nav-item" role="presentation">
-                                    <a data-mdb-tab-init class="nav-link active" id="ex1-tab-1" href="#ex1-tabs-1"
+                                    <a class="nav-link active" id="ex1-tab-1" data-mdb-toggle="tab" href="#ex1-tabs-1"
                                         role="tab" aria-controls="ex1-tabs-1" aria-selected="true">Today</a>
                                 </li>
                                 <li class="nav-item" role="presentation">
-                                    <a data-mdb-tab-init class="nav-link" id="ex1-tab-2" href="#ex1-tabs-2" role="tab"
-                                        aria-controls="ex1-tabs-2" aria-selected="false">This Month</a>
-                                </li class="nav-item">
-                                <li>
+                                    <a class="nav-link" id="ex1-tab-2" data-mdb-toggle="tab" href="#ex1-tabs-2"
+                                        role="tab" aria-controls="ex1-tabs-2" aria-selected="false">This Month</a>
+                                </li>
+                                <li class="nav-item">
                                     <form>
-                                        <div class="form-outline datepicker ecs-datepicker" >
+                                        <div class="form-outline datepicker ecs-datepicker">
                                             <input type="date" class="form-control" placeholder="Choose dates" />
                                         </div>
                                     </form>
                                 </li>
                             </ul>
-                            <!-- Tabs navs -->
                             <!-- Tabs content -->
                             <div class="tab-content" id="ex1-content">
                                 <div class="tab-pane fade show active" id="ex1-tabs-1" role="tabpanel"
@@ -68,7 +64,7 @@
                                     <p class="ecs-stats-num-txt2">Overall Earnings</p>
                                 </div>
                                 <div class="tab-pane fade" id="ex1-tabs-2" role="tabpanel" aria-labelledby="ex1-tab-2">
-                                    <p class="ecs-stats-num2">SAR {{ $fare }}</p>
+                                    <p class="ecs-stats-num2">SAR {{ $completed_earning_rides_this_month }}</p>
                                     <p class="ecs-stats-num-txt2">Overall Earnings</p>
                                 </div>
                             </div>
@@ -86,19 +82,18 @@
                             </div>
                             <div class="ecs-card-header-right">
                                 <a class="ecs-stats-btn" href="{{ route('rides') }}">View Rides</a>
-
                             </div>
                         </div>
                         <div class="card-body">
                             <!-- Tabs navs -->
-                            <ul class="nav nav-tabs mb-3" id="ex1" role="tablist">
+                            <ul class="nav nav-tabs mb-3" id="ex3" role="tablist">
                                 <li class="nav-item" role="presentation">
-                                    <a data-mdb-tab-init class="nav-link active" id="ex3-tab-3" href="#ex3-tabs-3"
+                                    <a class="nav-link active" id="ex3-tab-3" data-mdb-toggle="tab" href="#ex3-tabs-3"
                                         role="tab" aria-controls="ex3-tabs-3" aria-selected="true">Today</a>
                                 </li>
                                 <li class="nav-item" role="presentation">
-                                    <a data-mdb-tab-init class="nav-link" id="ex3-tab-2" href="#ex3-tabs-2" role="tab"
-                                        aria-controls="ex3-tabs-2" aria-selected="false">This Month</a>
+                                    <a class="nav-link" id="ex3-tab-2" data-mdb-toggle="tab" href="#ex3-tabs-2"
+                                        role="tab" aria-controls="ex3-tabs-2" aria-selected="false">This Month</a>
                                 </li>
                                 <li class="nav-item">
                                     <form>
@@ -108,41 +103,38 @@
                                     </form>
                                 </li>
                             </ul>
-                            <!-- Tabs navs -->
                             <!-- Tabs content -->
-                            <div class="tab-content" id="ex1-content">
+                            <div class="tab-content" id="ex3-content">
                                 <div class="tab-pane fade show active" id="ex3-tabs-3" role="tabpanel"
                                     aria-labelledby="ex3-tab-3">
                                     <div class="row">
                                         <div class="col-md-6">
-                                            <p class="ecs-stats-num2">{{ count($ride_created) }}</p>
+                                            <p class="ecs-stats-num2">{{ count($rides_created_today) }}</p>
                                             <p class="ecs-stats-num-txt2">Ride Created ></p>
                                         </div>
                                         <div class="col-md-6">
-                                            <p class="ecs-stats-num2">{{ count($completed_rides) }}</p>
+                                            <p class="ecs-stats-num2">{{ count($completed_rides_today) }}</p>
                                             <p class="ecs-stats-num-txt2">Completed ></p>
                                         </div>
                                         <div class="col-md-6">
-                                            <p class="ecs-stats-num2">{{ count($driver_assigned) }}</p>
+                                            <p class="ecs-stats-num2">{{ count($driver_assigned_today) }}</p>
                                             <p class="ecs-stats-num-txt2">Driver Assigned ></p>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="tab-pane fade" id="ex3-tabs-2" role="tabpanel" aria-labelledby="ex3-tab-2">
                                     <div class="row">
-                                        <div class="row">
-                                            <div class="col-md-6">
-                                                <p class="ecs-stats-num2">5,052</p>
-                                                <p class="ecs-stats-num-txt2">Ride Created ></p>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p class="ecs-stats-num2">232</p>
-                                                <p class="ecs-stats-num-txt2">Completed ></p>
-                                            </div>
-                                            <div class="col-md-6">
-                                                <p class="ecs-stats-num2">2,021</p>
-                                                <p class="ecs-stats-num-txt2">Driver Assigned ></p>
-                                            </div>
+                                        <div class="col-md-6">
+                                            <p class="ecs-stats-num2">{{ count($rides_created_this_month) }}</p>
+                                            <p class="ecs-stats-num-txt2">Ride Created ></p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p class="ecs-stats-num2">{{ count($completed_rides_this_month) }}</p>
+                                            <p class="ecs-stats-num-txt2">Completed ></p>
+                                        </div>
+                                        <div class="col-md-6">
+                                            <p class="ecs-stats-num2">{{ count($driver_assigned_this_month) }}</p>
+                                            <p class="ecs-stats-num-txt2">Driver Assigned ></p>
                                         </div>
                                     </div>
                                 </div>
@@ -175,16 +167,11 @@
                                 </tr>
                             </thead>
                             <tbody class="ecs-custom-body">
-
                                 @if (!empty($completed_rides))
                                     @foreach ($completed_rides as $value)
                                         <tr>
-                                            <td>
-                                                {{ $value->id }}
-                                            </td>
-                                            <td>
-                                                {{ $value->customer_name }}
-                                            </td>
+                                            <td>{{ $value->id }}</td>
+                                            <td>{{ $value->customer_name }}</td>
                                             <td>
                                                 <div class="dameentry">
                                                     <p class="datepart">{{ $value->booking_pickup }}</p>
@@ -194,64 +181,14 @@
                                             <td>{{ $value->hotel_pickup }}</td>
                                             <td>{{ $value->hotel_drop }}</td>
                                             <td>{{ $value->category }}</td>
+                                            {{-- <td>{{ $value->driver_name }}</td> --}}
                                             <td>Tauqeer Alam</td>
                                             <td>{{ $value->car_name }}</td>
-                                            <td class="status-success">Completed </td>
+                                            <td class="status-success">Completed</td>
                                             <td>{{ $value->fare }}</td>
                                         </tr>
                                     @endforeach
                                 @endif
-                                {{-- <tr>
-                                    <td>001</td>
-                                    <td>Tiger Nixon</td>
-                                    <td>12-06-2024</td>
-                                    <td>
-                                        Airport Road, King Khalid International Airport, Riyadh
-                                        Saudi Arabia
-                                    </td>
-                                    <td>Al Olaya, Riyadh Saudi Arabia</td>
-                                    <td class="type">Economy</td>
-                                    <td>Drake</td>
-                                    <td>Tahoe</td>
-                                    <td class="status-success">
-                                        Completed
-                                    </td>
-                                    <td>200 SAR</td>
-                                </tr>
-                                <tr>
-                                    <td>001</td>
-                                    <td>Tiger Nixon</td>
-                                    <td>12-06-2024</td>
-                                    <td>
-                                        Airport Road, King Khalid International Airport, Riyadh
-                                        Saudi Arabia
-                                    </td>
-                                    <td>Al Olaya, Riyadh Saudi Arabia</td>
-                                    <td class="type">Economy</td>
-                                    <td>Drake</td>
-                                    <td>Tahoe</td>
-                                    <td class="status-fail">
-                                        Driver Assigned
-                                    </td>
-                                    <td>200 SAR</td>
-                                </tr>
-                                <tr>
-                                    <td>001</td>
-                                    <td>Tiger Nixon</td>
-                                    <td>12-06-2024</td>
-                                    <td>
-                                        Airport Road, King Khalid International Airport, Riyadh
-                                        Saudi Arabia
-                                    </td>
-                                    <td>Al Olaya, Riyadh Saudi Arabia</td>
-                                    <td class="type">Economy</td>
-                                    <td>Drake</td>
-                                    <td>Tahoe</td>
-                                    <td class="status-warn">
-                                        Ride Created
-                                    </td>
-                                    <td>200 SAR</td>
-                                </tr> --}}
                             </tbody>
                         </table>
                     </div>
@@ -259,14 +196,11 @@
                         <p class="rows-txt">Rows per page</p>
                         <select class="custom-pages-ddl">
                             <option>25</option>
-                            <option>55</option>
+                            <option>50</option>
                             <option>75</option>
                         </select>
-                        <p class="rows-txt">1-75 of 89,33</p>
+                        <p class="rows-txt">1-75 of 8,933</p>
                         <div class="rows-clicks-main">
-
-
-
                             <img src="{{ asset('assets/icons/fast-left.png') }}" alt="arrow-fast-left" />
                             <img src="{{ asset('assets/icons/left.png') }}" alt="arrow-left" />
                             <img src="{{ asset('assets/icons/right.png') }}" alt="arrow-right" />
@@ -278,4 +212,8 @@
             <!-- TABLE ENDS -->
         </div>
     </main>
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/3.10.2/mdb.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js"></script>
 @endsection
